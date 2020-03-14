25475b28258d7697a78edc4fadba79e661bf465f
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 20:47:11 +0500

    Добавлена функция для выпадающего меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const showClubList = () => {
+  const body = document.body;
+
+  body.addEventListener('click', (event) => {
+    const clubList = document.querySelector('.club-select ul');
+    const target = event.target.closest('.club-select');
+
+    if (target) {
+      if (clubList.hasAttribute('style')) {
+        clubList.removeAttribute('style');
+      } else {
+        clubList.style.display = 'block';
+      }
+    } else {
+      clubList.removeAttribute('style');
+    }
+  });
+};
+
+showClubList();
