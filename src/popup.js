942f1a45e583cf81ddff7015a75ce1b246ebd5cf
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 21:50:35 +0500

    Функция модального окна вынесены в модуль

---
+++ src/popup.js
@@ -0,0 +1,21 @@
+const popup = () => {
+  const body = document.body;
+  let popup;
+
+  body.addEventListener('click', (event) => {
+    let target = event.target;
+
+    if (target.dataset.popup) {
+      event.preventDefault();
+      popup = document.querySelector(target.dataset.popup);
+      popup.style.display = 'block';
+      return;
+    }
+
+    if (target.closest('.close_icon') || !target.closest('.form-content')) {
+      popup.removeAttribute('style');
+    }
+  });
+};
+
+export default popup;
