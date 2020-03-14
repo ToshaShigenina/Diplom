19a7d2f6d503d7c60763e9761e22fdcae214722b
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 21:51:03 +0500

    Функция модального окна экспортирована

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 'use strict';
 
-/*import showClubList from 'showClubList.js';
+import showClubList from 'showClubList.js';
+import popup from 'popup.js';
 
-showClubList();*/
-
-const popup = () => {
-  const body = document.body;
-  let popup;
-
-  body.addEventListener('click', (event) => {
-    let target = event.target;
-
-    if (target.dataset.popup) {
-      event.preventDefault();
-      popup = document.querySelector(target.dataset.popup);
-      popup.style.display = 'block';
-      return;
-    }
-
-    if (target.closest('.close_icon') || !target.closest('.form-content')) {
-      popup.removeAttribute('style');
-    }
-  });
-};
+showClubList();
 
 popup();
