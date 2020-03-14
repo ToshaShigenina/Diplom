3054108257f83780814ae6cf635df5504d4cb805
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 21:47:40 +0500

    Добавлено закрытие модального окна

--- src/index.js
+++ src/index.js
@@ -4,21 +4,24 @@
 
 showClubList();*/
 
-const showPopUp = () => {
+const popup = () => {
   const body = document.body;
+  let popup;
 
   body.addEventListener('click', (event) => {
-    let target = event.target,
-      popup;
+    let target = event.target;
 
     if (target.dataset.popup) {
       event.preventDefault();
       popup = document.querySelector(target.dataset.popup);
       popup.style.display = 'block';
+      return;
     }
 
-
+    if (target.closest('.close_icon') || !target.closest('.form-content')) {
+      popup.removeAttribute('style');
+    }
   });
 };
 
-showPopUp();
+popup();
