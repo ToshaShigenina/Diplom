9635aa924235b56fa029428dc3dd0720baa7dbca
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 21:04:06 +0500

    Настроен экспорт функции

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 'use strict';
 
-const showClubList = () => {
-  const body = document.body;
-
-  body.addEventListener('click', (event) => {
-    const clubList = document.querySelector('.club-select ul');
-    const target = event.target.closest('.club-select');
-
-    if (target) {
-      if (clubList.hasAttribute('style')) {
-        clubList.removeAttribute('style');
-      } else {
-        clubList.style.display = 'block';
-      }
-    } else {
-      clubList.removeAttribute('style');
-    }
-  });
-};
+import showClubList from 'showClubList.js';
 
 showClubList();
+
+const showFreeVisitForm = () => {
+
+};
+
+showFreeVisitForm();
