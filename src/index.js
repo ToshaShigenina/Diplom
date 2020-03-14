ccecfa348e5f79405f90854839eddae001b28f20
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 23:03:15 +0500

    Подключены модули выпадающего меню и модальных окон

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-import showClubList from 'showClubList.js';
-import popup from 'popup.js';
+import showClubList from './showClubList.js';
+import popup from './popup.js';
 
 showClubList();
 
