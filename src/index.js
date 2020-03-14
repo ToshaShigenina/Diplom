cd795039843836b2128eaee1412b8108a4dc23c4
Tosha Shigenina <shigenina@gmail.com>
2020-03-14 21:21:31 +0500

    Добавлена функция для появления модальных окон

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 'use strict';
 
-import showClubList from 'showClubList.js';
+/*import showClubList from 'showClubList.js';
 
-showClubList();
+showClubList();*/
 
-const showFreeVisitForm = () => {
+const showPopUp = () => {
+  const body = document.body;
 
+  body.addEventListener('click', (event) => {
+    let target = event.target,
+      popup;
+
+    if (target.dataset.popup) {
+      event.preventDefault();
+      popup = document.querySelector(target.dataset.popup);
+      popup.style.display = 'block';
+    }
+
+
+  });
 };
 
-showFreeVisitForm();
+showPopUp();
