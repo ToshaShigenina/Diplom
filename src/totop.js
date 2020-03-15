11e383c267ea6a7487b3041ad7c6be8505a72124
Tosha Shigenina <shigenina@gmail.com>
2020-03-15 20:04:58 +0500

    Функция появления стрелки и прокрутки при клике

--- src/totop.js
+++ src/totop.js
@@ -1,8 +1,23 @@
 const totop = () => {
   const arrow = document.getElementById('totop');
+  let topHeader = document.querySelector('.header-main').clientHeight;
 
-  arrow.addEventListener('click', () => {
+  arrow.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.body.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth'
+    });
+  });
 
+  window.addEventListener('scroll', () => {
+    let topMain = window.scrollY;
+
+    if (topMain > (topHeader / 3) * 2) {
+      arrow.style.display = 'block';
+    } else {
+      arrow.removeAttribute('style');
+    }
   });
 };
 
