85cc0ccc26bcee86ff2dbcfa937affcf20a7cbed
Tosha Shigenina <shigenina@gmail.com>
2020-03-15 19:00:55 +0500

    Добавлена функция прикрепления меню к верхней части экрана

--- src/burgerMenu.js
+++ src/burgerMenu.js
@@ -1,35 +1,55 @@
 const burgerMenu = () => {
   const body = document.body,
-    popupMenu = document.querySelector('.popup-menu');
+    popupMenu = document.querySelector('.popup-menu'),
+    menu = document.querySelector('.top-menu'),
+    gift = document.querySelector('.fixed-gift');
 
-  body.addEventListener('click', (event) => {
-    let target = event.target;
+  let widthWindow = body.clientWidth;
 
-    if (target.matches('.close-menu-btn img') || target.matches('.scroll a')) {
-      popupMenu.removeAttribute('style');
-      return;
-    } else {
-      target = target.closest('.menu-button');
-      if (target) {
-        popupMenu.style.display = 'flex';
+  const doBurger = () => {
+    body.addEventListener('click', (event) => {
+      let target = event.target;
+
+      if (widthWindow < 768) {
+        if (target.matches('.close-menu-btn img') || target.matches('.scroll a')) {
+          popupMenu.removeAttribute('style');
+          return;
+        } else {
+          target = target.closest('.menu-button');
+          if (target) {
+            popupMenu.style.display = 'flex';
+          }
+        }
       }
-    }
-  });
+    });
 
-  document.documentElement.addEventListener('scroll', () => {
+    document.addEventListener('scroll', () => {
+      const topMenu = document.querySelector('.head').clientHeight;
+      let topMain = window.scrollY;
 
-    const menu = document.querySelector('.top-menu');
-    let topMain = body.scrollTop,
-      topMenu = menu.offsetTop;
-    console.log(topMain);
-    if (topMain >= topMenu) {
-      menu.style.position = 'fixed';
-      menu.style.top = '0';
-      menu.style.left = '0';
-    } else {
+      if (widthWindow < 768) {
+        if (topMain > topMenu) {
+          menu.style.position = 'fixed';
+          menu.style.top = '0';
+          menu.style.left = '0';
+          gift.style.top = '70px';
+          return;
+        }
+      }
+
       menu.removeAttribute('style');
-    }
+      gift.removeAttribute('style');
+    });
+  };
+
+  doBurger();
+
+  window.addEventListener('resize', () => {
+    widthWindow = body.clientWidth;
+    console.log(widthWindow);
+    doBurger();
   });
+
 };
 
 export default burgerMenu;
