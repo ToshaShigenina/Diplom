62c7a74ff92876dc4c704a6725561b2f4d00ad41
Tosha Shigenina <shigenina@gmail.com>
2020-03-17 02:22:42 +0500

    Добавлена функция рассчета стоимости + промокод

--- src/calc.js
+++ src/calc.js
@@ -1,18 +1,47 @@
 const calc = () => {
-  const priceList = {
-    clubMozaika: [
-      1990,
-      9900,
-      13900,
-      19900
-    ],
-    clubSchelkovo: [
-      2990,
-      14990,
-      21990,
-      24990
-    ]
-  }
+  const price = {
+    mozaika: {
+      1: 1990,
+      6: 9900,
+      9: 13900,
+      12: 19900
+    },
+    schelkovo: {
+      1: 2990,
+      6: 14990,
+      9: 21990,
+      12: 24990
+    }
+  };
+
+  const priceTotal = document.getElementById('price-total'),
+    cardOrder = document.getElementById('card_order');
+
+  let month = 1,
+    club = 'mozaika';
+
+  const calcPrice = (event) => {
+    let target = event.target;
+    const promo = cardOrder.querySelector('[name="name"]');
+
+    if (target.matches('[name="club-name"]')) {
+      club = target.value;
+    }
+    if (target.matches('[name="card-type"]')) {
+      month = target.value;
+    }
+
+    if (promo.value !== '' || promo === target) {
+      if (promo.value === 'ТЕЛО2019') {
+        priceTotal.textContent = Math.floor((price[club][month] / 100) * 70);
+        return;
+      }
+    }
+
+    priceTotal.textContent = price[club][month];
+  };
+
+  cardOrder.addEventListener('change', calcPrice);
 };
 
 export default calc;
