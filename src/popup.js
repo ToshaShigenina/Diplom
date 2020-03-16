ee9123e022596759dcca001e530f1d054cd7d84e
Tosha Shigenina <shigenina@gmail.com>
2020-03-17 00:22:21 +0500

    Исправлена ошибка в функции модальных окон, проект пересобран

--- src/popup.js
+++ src/popup.js
@@ -9,7 +9,9 @@ const popup = () => {
       target = target.closest('[data-popup]');
       if (target) {
         event.preventDefault();
-        document.querySelector('[data-popup="#gift"]').style.display = 'none';
+        if (document.querySelector('[data-popup="#gift"]')) {
+          document.querySelector('[data-popup="#gift"]').style.display = 'none';
+        }
         popup = document.querySelector(target.dataset.popup);
         popup.style.display = 'block';
       }
@@ -17,7 +19,9 @@ const popup = () => {
     }
 
     if (target.closest('.close_icon') || target.closest('.close-btn') || !target.closest('.form-content')) {
-      document.querySelector('[data-popup="#gift"]').removeAttribute('style');
+      if (document.querySelector('[data-popup="#gift"]')) {
+        document.querySelector('[data-popup="#gift"]').removeAttribute('style');
+      }
       popup.removeAttribute('style');
       popup = null;
     }
