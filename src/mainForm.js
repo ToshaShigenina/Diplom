8394f5dd1cd1c7207d9cbb696f92d40e9b22c247
Tosha Shigenina <shigenina@gmail.com>
2020-03-18 03:00:59 +0500

    Добавлены формы из модальных окон

--- src/mainForm.js
+++ src/mainForm.js
@@ -1,6 +1,14 @@
 const mainForm = () => {
   const bannerForm = document.getElementById('banner-form'),
-    thanks = document.getElementById('thanks');
+    cardOrder = document.getElementById('card_order'),
+    modalForm = document.getElementById('form2'),
+    callForm = document.getElementById('form1'),
+    thanks = document.getElementById('thanks'),
+    visit = document.getElementById('free_visit_form'),
+    callFree = document.getElementById('callback_form'),
+    messageSend = 'Ваша заявка отправлена. <br> Мы свяжемся с вами в ближайшее время.',
+    messageError = 'Произошла ошибка при отправке данных',
+    formError = 'Необходимо подтвердить согласие на обработку персональных данных!';
 
   const error = document.createElement('p');
   error.style.cssText = `color: red; margin-top: 1rem; font-size: 110%; text-align: center`;
@@ -26,7 +34,7 @@ const mainForm = () => {
         }
       });
     },
-    readForm = (form, event) => {
+    readForm = (form, event, callback) => {
       const formData = new FormData(form);
       let body = {};
 
@@ -36,36 +44,83 @@ const mainForm = () => {
 
       postData(body)
         .then((response) => {
-          thanks.querySelector('h4').textContent = 'Подождите';
-          thanks.querySelector('p').innerHTML = '';
-          if (response.status !== 200) {
-            thanks.querySelector('h4').textContent = 'Сожалеем';
-            thanks.querySelector('p').innerHTML = 'Произошла ошибка при отправке данных';
-          } else {
-            thanks.querySelector('h4').textContent = 'Спасибо!';
-            thanks.querySelector('p').innerHTML = 'Ваша заявка отправлена. <br> Мы свяжемся с вами в ближайшее время.';
-          }
-          thanks.querySelector('button').style.display = 'block';
+          callback(response);
         });
       clearInput(form);
-    };;
+    },
+    personalData = (event, selector) => {
+      let target = event.target.closest(selector);
+      if (target && target.checked) {
+        error.remove();
+      }
+    };
 
   bannerForm.addEventListener('submit', (event) => {
     event.preventDefault();
     if (document.getElementById('check1').checked) {
-      readForm(bannerForm, event);
+      readForm(bannerForm, event, (response) => {
+        thanks.querySelector('h4').textContent = 'Подождите';
+        thanks.querySelector('p').innerHTML = '';
+        if (response.status !== 200) {
+          thanks.querySelector('h4').textContent = 'Сожалеем';
+          thanks.querySelector('p').innerHTML = messageError;
+        } else {
+          thanks.querySelector('h4').textContent = 'Спасибо!';
+          thanks.querySelector('p').innerHTML = messageSend;
+        }
+        thanks.querySelector('button').style.display = 'block';
+      });
     } else {
-      error.textContent = 'Необходимо подтвердить согласие на обработку персональных данных!';
+      error.textContent = formError;
       bannerForm.append(error);
+    }
+  });
+  modalForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (document.getElementById('check2').checked) {
+      readForm(modalForm, event, (response) => {
+        visit.querySelector('.form-content').innerHTML = '<h4>Подождите</h4><p></p>';
+        if (response.status !== 200) {
+          visit.querySelector('h4').textContent = 'Сожалеем';
+          visit.querySelector('p').innerHTML = messageError;
+        } else {
+          visit.querySelector('h4').textContent = 'Спасибо!';
+          visit.querySelector('p').innerHTML = messageSend;
+        }
+      });
+    } else {
+      error.textContent = formError;
+      modalForm.append(error);
+    }
+  });
+  callForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (document.getElementById('check').checked) {
+      readForm(callForm, event, (response) => {
+        callFree.querySelector('.form-content').innerHTML = '<h4>Подождите</h4><p></p>';
+        if (response.status !== 200) {
+          callFree.querySelector('h4').textContent = 'Сожалеем';
+          callFree.querySelector('p').innerHTML = messageError;
+        } else {
+          callFree.querySelector('h4').textContent = 'Спасибо!';
+          callFree.querySelector('p').innerHTML = messageSend;
+        }
+      });
+    } else {
+      error.textContent = formError;
+      callForm.append(error);
     }
   });
 
   bannerForm.addEventListener('change', (event) => {
-    let target = event.target.closest('#check1');
-    if (target && target.checked) {
-      error.remove();
-    }
-  })
+    personalData(event, '#check1')
+  });
+  modalForm.addEventListener('change', (event) => {
+    personalData(event, '#check2')
+  });
+  callForm.addEventListener('change', (event) => {
+    personalData(event, '#check')
+  });
 };
 
 export default mainForm;
