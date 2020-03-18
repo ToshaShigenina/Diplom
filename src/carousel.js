6ac4d91659b5fb9ab419f93e33f826a7796c3315
Tosha Shigenina <shigenina@gmail.com>
2020-03-19 03:17:21 +0500

    Карусель

--- src/carousel.js
+++ src/carousel.js
@@ -1,5 +1,62 @@
 const carousel = () => {
+  const carouselSlider = document.querySelector('.services-slider'),
+    carouselSliderWrapper = document.querySelector('.services-slider-wrapper'),
+    slides = carouselSlider.querySelectorAll('.slide'),
+    slideWidth = slides[0].clientWidth,
+    widthSlider = (slideWidth + 12) * 10;
 
+  let position = 1;
+
+  const nextSlider = (slider) => {
+      position++;
+      let currentWidthSlides = (slideWidth + 12) * position,
+        diffWidth = currentWidthSlides + slider.clientWidth - widthSlider;
+      if (diffWidth > 0) {
+        currentWidthSlides = ((slideWidth + 12) * (position - 1)) + (slideWidth - diffWidth);
+        position = 0;
+      } else if (diffWidth === 0) {
+        position = 0;
+      }
+      slider.style.transform = `translateX(-${currentWidthSlides}px)`;
+    },
+    prevSlider = (slider) => {
+      position--;
+      let currentWidthSlides = (slideWidth + 12) * position,
+        diffWidth = currentWidthSlides + slider.clientWidth - widthSlider;
+      if (position < 0) {
+        currentWidthSlides = ((slideWidth + 12) * (position - 1)) + (slideWidth - diffWidth);
+        position = 0;
+      }
+      slider.style.transform = `translateX(-${currentWidthSlides}px)`;
+    },
+    startSlider = (slider) => {
+      setInterval(nextSlider, 1000, slider);
+    },
+    addArrows = (slider) => {
+      const arrowNext = document.createElement('div'),
+        arrowPrev = document.createElement('div');
+      arrowNext.classList.add('slider-arrow', 'next');
+      arrowPrev.classList.add('slider-arrow', 'prev');
+      arrowNext.innerHTML = `<span><i class="fa fa-chevron-right"></i></span>`;
+      arrowPrev.innerHTML = `<span><i class="fa fa-chevron-left"></i></span>`;
+      slider.append(arrowNext);
+      slider.append(arrowPrev);
+    };
+
+  addArrows(carouselSliderWrapper);
+  startSlider(carouselSlider);
+
+  carouselSlider.addEventListener('click', (event) => {
+    event.preventDefault();
+    let target = event.target;
+
+    if (target.matches('.next')) {
+      nextSlider(carouselSlider);
+    }
+    /*if (target.matches('.prev')) {
+      prevSlider(carouselSlider);
+    }*/
+  });
 };
 
 export default carousel;
