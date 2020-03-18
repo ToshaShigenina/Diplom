e99c0a1b5ebf2e1db00b7f3c594f43d5bd8d3484
Tosha Shigenina <shigenina@gmail.com>
2020-03-18 20:50:14 +0500

    В html добавлены дата-атрибуты, в js функция для формы клубов

--- src/mainForm.js
+++ src/mainForm.js
@@ -6,7 +6,7 @@ const mainForm = () => {
     callForm = document.getElementById('form1'), // форма
     callFree = document.getElementById('callback_form'), // элемент для вывода сообщения
     footerForm = document.getElementById('footer_form'), // форма, элемент для вывода сообщения thanks
-    cardOrderForm = document.getElementById('card_order'),
+    cardOrderForm = document.getElementById('card_order'), // форма, элемент для вывода сообщения thanks
     messageSend = 'Ваша заявка отправлена. <br> Мы свяжемся с вами в ближайшее время.',
     messageError = 'Произошла ошибка при отправке данных',
     formError = 'Необходимо подтвердить согласие на обработку персональных данных!',
@@ -55,22 +55,25 @@ const mainForm = () => {
       if (target && target.checked) {
         error.remove();
       }
+    },
+    popupMessage = (modal, response) => {
+      modal.querySelector('.form-content').innerHTML = `<h4>Подождите</h4><p></p><button class="btn close-btn" style="display: none;">OK</button>`;
+      if (response.status !== 200) {
+        modal.querySelector('h4').textContent = 'Сожалеем';
+        modal.querySelector('p').innerHTML = messageError;
+      } else {
+        modal.querySelector('h4').textContent = 'Спасибо!';
+        modal.querySelector('p').innerHTML = messageSend;
+      }
+
+      modal.querySelector('button').style.display = 'block';
     };
 
   bannerForm.addEventListener('submit', (event) => {
     event.preventDefault();
     if (document.getElementById('check1').checked) {
       readForm(bannerForm, event, (response) => {
-        thanks.querySelector('h4').textContent = 'Подождите';
-        thanks.querySelector('p').innerHTML = '';
-        if (response.status !== 200) {
-          thanks.querySelector('h4').textContent = 'Сожалеем';
-          thanks.querySelector('p').innerHTML = messageError;
-        } else {
-          thanks.querySelector('h4').textContent = 'Спасибо!';
-          thanks.querySelector('p').innerHTML = messageSend;
-        }
-        thanks.querySelector('button').style.display = 'block';
+        popupMessage(thanks, response);
       });
     } else {
       error.textContent = formError;
@@ -81,16 +84,7 @@ const mainForm = () => {
     event.preventDefault();
     if (document.getElementById('footer_leto_mozaika').checked || document.getElementById('footer_leto_schelkovo').checked) {
       readForm(footerForm, event, (response) => {
-        thanks.querySelector('h4').textContent = 'Подождите';
-        thanks.querySelector('p').innerHTML = '';
-        if (response.status !== 200) {
-          thanks.querySelector('h4').textContent = 'Сожалеем';
-          thanks.querySelector('p').innerHTML = messageError;
-        } else {
-          thanks.querySelector('h4').textContent = 'Спасибо!';
-          thanks.querySelector('p').innerHTML = messageSend;
-        }
-        thanks.querySelector('button').style.display = 'block';
+        popupMessage(thanks, response);
       });
     } else {
       error.textContent = footerError;
@@ -101,14 +95,7 @@ const mainForm = () => {
     event.preventDefault();
     if (document.getElementById('check2').checked) {
       readForm(modalForm, event, (response) => {
-        visit.querySelector('.form-content').innerHTML = '<h4>Подождите</h4><p></p>';
-        if (response.status !== 200) {
-          visit.querySelector('h4').textContent = 'Сожалеем';
-          visit.querySelector('p').innerHTML = messageError;
-        } else {
-          visit.querySelector('h4').textContent = 'Спасибо!';
-          visit.querySelector('p').innerHTML = messageSend;
-        }
+        popupMessage(visit, response);
       });
     } else {
       error.textContent = formError;
@@ -119,19 +106,16 @@ const mainForm = () => {
     event.preventDefault();
     if (document.getElementById('check').checked) {
       readForm(callForm, event, (response) => {
-        callFree.querySelector('.form-content').innerHTML = '<h4>Подождите</h4><p></p>';
-        if (response.status !== 200) {
-          callFree.querySelector('h4').textContent = 'Сожалеем';
-          callFree.querySelector('p').innerHTML = messageError;
-        } else {
-          callFree.querySelector('h4').textContent = 'Спасибо!';
-          callFree.querySelector('p').innerHTML = messageSend;
-        }
+        popupMessage(callFree, response);
       });
     } else {
       error.textContent = formError;
       callForm.append(error);
     }
+  });
+  cardOrderForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
   });
 
   bannerForm.addEventListener('change', (event) => {
