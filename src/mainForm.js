3976d6b571c2bb6c08998456e103a5f54e59f448
Tosha Shigenina <shigenina@gmail.com>
2020-03-18 19:50:32 +0500

    Добавлена функция для формы в футере

--- src/mainForm.js
+++ src/mainForm.js
@@ -1,14 +1,16 @@
 const mainForm = () => {
-  const bannerForm = document.getElementById('banner-form'),
-    cardOrder = document.getElementById('card_order'),
-    modalForm = document.getElementById('form2'),
-    callForm = document.getElementById('form1'),
-    thanks = document.getElementById('thanks'),
-    visit = document.getElementById('free_visit_form'),
-    callFree = document.getElementById('callback_form'),
+  const modalForm = document.getElementById('form2'), // форма
+    visit = document.getElementById('free_visit_form'), // элемент для вывода сообщения
+    bannerForm = document.getElementById('banner-form'), // форма
+    thanks = document.getElementById('thanks'), // элемент для вывода сообщения
+    callForm = document.getElementById('form1'), // форма
+    callFree = document.getElementById('callback_form'), // элемент для вывода сообщения
+    footerForm = document.getElementById('footer_form'), // форма, элемент для вывода сообщения thanks
+    cardOrderForm = document.getElementById('card_order'),
     messageSend = 'Ваша заявка отправлена. <br> Мы свяжемся с вами в ближайшее время.',
     messageError = 'Произошла ошибка при отправке данных',
-    formError = 'Необходимо подтвердить согласие на обработку персональных данных!';
+    formError = 'Необходимо подтвердить согласие на обработку персональных данных!',
+    footerError = 'Необходимо выбрать клуб!';
 
   const error = document.createElement('p');
   error.style.cssText = `color: red; margin-top: 1rem; font-size: 110%; text-align: center`;
@@ -75,6 +77,26 @@ const mainForm = () => {
       bannerForm.append(error);
     }
   });
+  footerForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (document.getElementById('footer_leto_mozaika').checked || document.getElementById('footer_leto_schelkovo').checked) {
+      readForm(footerForm, event, (response) => {
+        thanks.querySelector('h4').textContent = 'Подождите';
+        thanks.querySelector('p').innerHTML = '';
+        if (response.status !== 200) {
+          thanks.querySelector('h4').textContent = 'Сожалеем';
+          thanks.querySelector('p').innerHTML = messageError;
+        } else {
+          thanks.querySelector('h4').textContent = 'Спасибо!';
+          thanks.querySelector('p').innerHTML = messageSend;
+        }
+        thanks.querySelector('button').style.display = 'block';
+      });
+    } else {
+      error.textContent = footerError;
+      footerForm.append(error);
+    }
+  });
   modalForm.addEventListener('submit', (event) => {
     event.preventDefault();
     if (document.getElementById('check2').checked) {
@@ -114,6 +136,10 @@ const mainForm = () => {
 
   bannerForm.addEventListener('change', (event) => {
     personalData(event, '#check1')
+  });
+  footerForm.addEventListener('change', (event) => {
+    personalData(event, '#footer_leto_mozaika');
+    personalData(event, '#footer_leto_schelkovo');
   });
   modalForm.addEventListener('change', (event) => {
     personalData(event, '#check2')
